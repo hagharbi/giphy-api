daf9c514c19e7edfda96381dd04b215d9b6206dd
Santino <ha.agharbi@gmail.com>
2019-01-17 12:15:54 -0800

    optimized layout style and js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,20 +3,25 @@ $(document).ready(function() {
 
 var topics = ["Chihuahua", "German Shepard", "Bulldog", "Doberman", "Husky"];
 
+    // function to display gifs
     function displayStaticGif() {
-
+        // deletes gifs prior to adding new ones
         $("#display-images").empty();
+
         var input = $(this).attr("data-name");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
             input + "&api_key=wFV6QaGmYZO3FYZNB3KGWcg4mBAd4tJq&limit=10";
 
+        // creates ajax call
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response) {
-
+            
+            // for loop goes through each gif and adds these variables
             for(var i = 0; i < response.data.length; i++) {
                 
+                // create a div to hold the results
                 var displayDiv = $("<div>");
                 displayDiv.addClass("holder");
 
@@ -25,11 +30,11 @@ var topics = ["Chihuahua", "German Shepard", "Bulldog", "Doberman", "Husky"];
                 image.attr("data-still", response.data[i].images.fixed_height_still.url);
                 image.attr("data-animate", response.data[i].images.fixed_height.url);
                 image.attr("data-state", "still");
-                image.attr("class", "gif");
+                image.addClass("gif");
                 displayDiv.append(image);
 
                 var rating = response.data[i].rating;
-                displayDiv.append(rating);
+                displayDiv.append("<p style='color: #3fbf56; font-weight: bold; text-align: center'> Rating: " + rating + "</p>");
 
                 $("#display-images").append(displayDiv);
                 console.log(response);
@@ -37,20 +42,22 @@ var topics = ["Chihuahua", "German Shepard", "Bulldog", "Doberman", "Husky"];
         });
     }
 
+    // function to create buttons for each array(original ones and the ones to be)
     function renderButtons() {
-
+        // deletes original buttons so there no repeats when new button is generated
         $("#display-buttons").empty();
-
+        // loops through the topics array
         for (var j = 0; j < topics.length; j++) {
 
             var newButton = $("<button>");
-            newButton.attr("data-name", topics[j]);
-            newButton.text(topics[j]);
+            newButton.attr("data-name", topics[j]); // add a data-attribute
+            newButton.text(topics[j]); // assigns button text
             $("#display-buttons").append(newButton);
         }
 
     }
 
+    // function to animate gifs
     function imageChangeState() {
 
         var state = $(this).attr("data-state");
@@ -69,17 +76,25 @@ var topics = ["Chihuahua", "German Shepard", "Bulldog", "Doberman", "Husky"];
     $("#submitButton").on("click", function(event) {
         event.preventDefault();
 
+        // grabs the user input value
         var input = $("#user-input").val().trim();
-        form.reset();
+
+        // the input now is added to the array
         topics.push(input);
 
+        // function is called, which makes buttons for all my shows plus the user show
         renderButtons();
 
+        // this line is so users can hit "enter" instead of clicking the submit button
+        return false; 
     });
 
     renderButtons();
 
+    // function for displaying gif on button click
     $(document).on("click", "button", displayStaticGif);
+
+    // function to animate gif on click
     $(document).on("click", ".gif", imageChangeState);
 
  
